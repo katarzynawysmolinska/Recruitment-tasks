ee301eae437578cd7ef696e475885ffa28c10267
Katarzyna <katarzyna.wysmolinska1@gmail.com>
2021-12-09 13:04:35 +0100

    Got elements by id and prevent overload of form

--- Zadanie2/script.js
+++ Zadanie2/script.js
@@ -1,1 +1,9 @@
+const form = document.getElementById("form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const priority = document.getElementById("priority");
+const category = document.getElementById("category");
 
+form.addEventListener("submit", e => {
+	e.preventDefault();
+});
